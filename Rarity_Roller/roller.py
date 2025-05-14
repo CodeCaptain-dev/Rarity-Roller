900b43a757dd85e23f7906ff2c1c3e6127a61344
CodeCaptain-dev <hasandu.nimsara@gmail.com>
2025-05-13 18:35:32 -0700

    Add files via upload

---
+++ Rarity_Roller/roller.py
@@ -0,0 +1,191 @@
+import random
+import time
+
+print("Welcome to Rarity Roller")
+print("")
+print("Known rarities are: Basic, Common, Uncommon, Rare, Epic, Legendary, Mythic")
+print("")
+print("If you get a rarity, you will also get the rarity tiers below it.")
+print("")
+print("There are more secret rarities you can find.")
+print("")
+print("Good luck, you will need it.")
+print("-------------------------------------------------")
+print("Hold Enter or Press R to Roll")
+while True:
+    iput = ""
+    iput = input()
+    if iput == "r" or "R":
+        print("")
+        print("Rolling...")
+        print("")
+        print("")
+        common = random.randint(1, 2)
+        if common == 1:
+            print("Roll Complete")
+            print("Final Rarity = Basic, 50%")
+
+            print("")
+            uncommon = random.randint(1, 2)
+        if common == 2:
+            print("Roll Complete")
+            print("Final Rarity = Common, 50%")
+
+            print("")
+            uncommon = random.randint(1, 2)
+
+            if uncommon == 2:
+                print("")
+                print("Roll Complete")
+                print("Final Rarity = Uncommon, 25%")
+
+                print("")
+                rare = random.randint(1, 2)
+
+                if rare == 2:
+                    print("")
+                    print("Roll Complete")
+                    print("Final Rarity = Rare, 12.5%")
+
+                    print("")
+                    super_rare = random.randint(1, 2)
+
+                    if super_rare == 2:
+                        print("")
+                        print("Roll Complete")
+                        print("Final Rarity = Super Rare, 6.25%")
+
+                        print("")
+                        epic = random.randint(1, 2)
+
+                        if epic == 2:
+                            print("")
+                            print("Roll Complete")
+                            print("Final Rarity = Epic, 3.125%")
+
+                            print("")
+                            legendary = random.randint(1, 2)
+
+                            if legendary == 2:
+                                print("")
+                                print("Roll Complete !RARITY ALERT!")
+                                print("Final Rarity = Legendary, 1.56%")
+
+                                print("")
+                                mythic = random.randint(1, 2)
+
+                                if mythic == 2:
+                                    print("")
+                                    print("Roll Complete !RARITY ALERT!")
+                                    print("Final Rarity = Mythic, 0.78%")
+
+                                    print("")
+                                    exotic = random.randint(1, 2)
+
+                                    if exotic == 2:
+                                        print("")
+                                        print("Roll Complete !RARITY ALERT!")
+                                        print("Final Rarity = Exotic, 0.39%")
+
+                                        print("")
+                                        insane = random.randint(1, 2)
+
+                                        if insane == 2:
+                                            print("")
+                                            print("Roll Complete !RARITY ALERT!")
+                                            print("Final Rarity = Insane, 0.195%")
+
+                                            print("")
+                                            chroma = random.randint(1, 2)
+
+                                            if chroma == 2:
+                                                print("")
+                                                print("Roll Complete !RARITY ALERT!")
+                                                print("Final Rarity = Chromatic, 0.0975%")
+                                                print("You have been playing this game a bit too much...")
+
+                                                print("")
+                                                unreal = random.randint(1, 2)
+
+                                                if unreal == 2:
+                                                    print("")
+                                                    print("Roll Complete !RARITY ALERT!")
+                                                    print("Final Rarity = Unreal, 0.05%")
+
+                                                    nuclear = random.randint(1, 2)
+
+                                                    if nuclear == 2:
+                                                        print("")
+                                                        print("Roll Complete !RARITY ALERT!")
+                                                        print("Final Rarity = Nuclear, 0.025%")
+
+                                                        print("")
+                                                        alien = random.randint(1, 2)
+
+                                                        if alien == 2:
+                                                            print("")
+                                                            print("Roll Complete !RARITY ALERT!")
+                                                            print("Final Rarity = Alien, 0.0125%")
+
+                                                            print("")
+                                                            uni = random.randint(1, 2)
+
+                                                            if uni == 2:
+                                                                print("Roll Complete !RARITY ALERT!")
+                                                                print("Final Rarity = Universal")
+                                                                print("Final Rarity = 0.00625%")
+
+                                                                print("")
+                                                                error = random.randint(1, 2)
+
+                                                                if error == 2:
+                                                                    print("R0!! Co)*@#####")
+                                                                    print("f!n@l R@arasity = Final Rarity")
+                                                                    print("FATAL ERRORRDS")
+                                                                    print("Traceback (most recent call last):")
+                                                                    print("File main.py line 107")
+                                                                    print("SyntaxError: 'break' outside loop")
+                                                                    print("0.003125%")
+
+                                                                    impossible = random.randint(1, 2)
+
+                                                                    if impossible == 2:
+                                                                        print("")
+                                                                        print("Roll Complete???????????")
+                                                                        print("Final Rarity = Imposssible?")
+                                                                        print("How did you get this?")
+                                                                        print("0.0015625% Rarity")
+                                                                        print("+1000 Credits, JACKPOT")
+                                                                        secret = random.randint(1, 2)
+
+                                                                        if secret == 2:
+                                                                            print("???????")
+                                                                            print("????????")
+                                                                            print("0.00078125? ******** ERROR")
+                                                                            print("you win!!!!!!")
+                                                                            print("your reward is: &@(#Y)(@ @#( ")
+                                                                            print("yhypaf yvssly")
+                                                                            print("aohurz mvy wshfpun")
+                                                                            time.sleep(2.5)
+                                                                            print("The Final Rarity has been revealed.")
+                                                                            print(
+                                                                                "After all of the time you have spent on this game.")
+                                                                            print(
+                                                                                "You have finally gained the secrets of this game.")
+                                                                            print(
+                                                                                "This game was meant for fun, but you...")
+                                                                            print("")
+                                                                            print(
+                                                                                "You took your luck and patience to the extreme to gain this rarity. ")
+
+                                                                            print("Congratulations...")
+                                                                            break
+                                                                            print("error")
+                                                                            time.sleep(1)
+                                                                            while True:
+                                                                                print("Congratulations...")
+
+
+
+
+
